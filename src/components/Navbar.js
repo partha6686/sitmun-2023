b79c8c43169a09cf0bb0f14a0cb369d0acb22974
Partha Sarathi Praharaj <parthasarathi6686@gmail.com>
2022-12-03 12:25:26 +0530

    initial setup and file structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import logo from "../img/logo.png";
+import "../css/navbar.css";
+
+function NavbarSection() {
+  const [navBackground, setNavBackground] = useState(false);
+  const setNavbar = () => {
+    if (window.scrollY > 86) {
+      setNavBackground(true);
+    } else {
+      setNavBackground(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", setNavbar);
+    // eslint-disable-next-line
+  }, []);
+  return (
+    <Navbar
+      variant={`${navBackground ? "dark" : "light"}`}
+      expand="md"
+      fixed="top"
+      className={`navbar ${navBackground ? "solid" : "transparent"} `}
+    >
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="60"
+            height="60"
+            className="d-inline-block align-top"
+            alt="SITMUN logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav.Link href="/" className="navbar-menu">
+            HOME
+          </Nav.Link>
+          <Nav.Link href="/teams" className="navbar-menu">
+            TEAM
+          </Nav.Link>
+          <Nav.Link href="#home" className="navbar-menu">
+            COMMITTEE
+          </Nav.Link>
+          <Nav.Link href="#home" className="navbar-menu-button">
+            <Button className="register-button">REGISTER</Button>
+          </Nav.Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarSection;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div>Welcome to SITMUN 2023</div>
+    </div>
+  );
+};
+
+export default Home;
