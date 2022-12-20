60327e338d09b7f24f12ba6b03c7a2165a9d0633
Partha Sarathi Praharaj <parthasarathi6686@gmail.com>
2022-12-20 15:07:08 +0530

    added lazy loadig to app and home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React, { Suspense } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Navbar from "./components/common/Navbar";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import TeamPage from "./pages/Team";
-import CommitteePage from "./pages/CommitteePage";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import Navbar from "./components/common/Navbar";
+// import Home from "./pages/Home";
+// import TeamPage from "./pages/Team";
+// import CommitteePage from "./pages/CommitteePage";
+const Navbar = React.lazy(() => import("./components/common/Navbar"));
+const Home = React.lazy(() => import("./pages/Home"));
+const TeamPage = React.lazy(() => import("./pages/Team"));
+const CommitteePage = React.lazy(() => import("./pages/CommitteePage"));
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/teams" element={<TeamPage />} />
-          <Route path="/committee" element={<CommitteePage />} />
-        </Routes>
+        <Suspense fallback={<div style={{ color: "white" }}>Loading...</div>}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/teams" element={<TeamPage />} />
+            <Route path="/committee" element={<CommitteePage />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
