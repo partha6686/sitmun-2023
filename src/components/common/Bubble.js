6a581fb3c724380b1a086192be3bb812187f7606
Partha Sarathi Praharaj <parthasarathi6686@gmail.com>
2022-12-20 13:37:27 +0530

    bubble component

---
+++ src/components/common/Bubble.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Bubble = () => {
+  return (
+    <section class="sticky">
+      <div class="bubbles">
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        <div class="bubble"></div>
+        
+      </div>
+    </section>
+  );
+};
+
+export default Bubble;
